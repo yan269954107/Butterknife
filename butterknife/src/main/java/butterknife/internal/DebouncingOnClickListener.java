530674cda7ab498d58864a06bc3b05c88cbffcc6
angeloyxw <angeloyxw@gmail.com>
2018-05-07 10:19:24 +0800

    init

---
+++ butterknife/src/main/java/butterknife/internal/DebouncingOnClickListener.java
@@ -0,0 +1,23 @@
+package butterknife.internal;
+
+import android.view.View;
+
+/**
+ * A {@linkplain View.OnClickListener click listener} that debounces multiple clicks posted in the
+ * same frame. A click on one button disables all buttons for that frame.
+ */
+public abstract class DebouncingOnClickListener implements View.OnClickListener {
+
+  static long clickTime = 0;
+
+  @Override
+  public final void onClick(View v) {
+    long time = System.currentTimeMillis();
+    if (time - clickTime < 500) return;
+    clickTime = time;
+    doClick(v);
+  }
+
+  public abstract void doClick(View v);
+}
+
